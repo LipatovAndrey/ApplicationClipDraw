fef409422c2de0deba9870e2e81d30bb0977d911
LipatovAndrey <lipa-andrej@yandex.ru>
2017-04-17 22:04:30 +0300

    Version with two green battary

--- app/src/main/java/com/example/applicationclipdraw/MainActivity.java
+++ app/src/main/java/com/example/applicationclipdraw/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.applicationclipdraw;
 
 import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.icu.util.ValueIterator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,23 +15,29 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    View second;
+    ImageView imageView, imageView1;
+    View secondLayerlistView;
     ClipDrawable clipDrawable, clipDrawable2;
     ValueAnimator mAnimator;
+    LayerDrawable layerDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        secondLayerlistView = findViewById(R.id.secondLayerlistView);
         imageView = (ImageView) findViewById(R.id.imageView);
 
+
+        layerDrawable = (LayerDrawable) secondLayerlistView.getBackground();
+        clipDrawable2 = (ClipDrawable) layerDrawable.getDrawable(2);
         // Создаём аниматор
         clipDrawable = (ClipDrawable) imageView.getDrawable();
-        //clipDrawable2 = (ClipDrawable) getResources().getDrawable(R.drawable.clip_second);
+
 
         mAnimator = ValueAnimator.ofInt(0, 10000);
-        mAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
         mAnimator.setRepeatCount(ValueAnimator.INFINITE);
         mAnimator.setDuration(5000);
         mAnimator.setInterpolator(new DecelerateInterpolator());
@@ -37,10 +46,12 @@ public class MainActivity extends AppCompatActivity {
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 Integer value = (Integer) valueAnimator.getAnimatedValue();
                 clipDrawable.setLevel(value);
+                clipDrawable2.setLevel(value);
 
             }
         });
         mAnimator.start();
 
     }
+
 }
